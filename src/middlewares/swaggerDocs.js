7131209e48f4d2df4523a1373b7a3645608b3ecb
MazaFaaka <haitovvasyl@gmail.com>
2025-08-10 13:50:40 +0200

    again fix prb nameing

--- src/middlewares/swaggerDocs.js
+++ src/middlewares/swaggerDocs.js
@@ -3,7 +3,7 @@ import swaggerUI from 'swagger-ui-express';
 import fs from 'node:fs';
 import { SWAGGER_PATH } from '../constants/index.js';
 
-export const swaggerDoc = (app) => {
+export const swaggerDocs = (app) => {
   try {
     const swaggerDoc = JSON.parse(fs.readFileSync(SWAGGER_PATH).toString());
     return [...swaggerUI.serve, swaggerUI.setup(swaggerDoc)];
